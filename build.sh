5236db76af1e3719b9bc4d4ec01a8b06ffdcc767
John Nguyen <jtnguyen236@gmail.com>
2018-01-05 09:36:48 +0800

    updates build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gox -output="./dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -os="linux" ./cmd/kk/
+gox -arch="amd64" -os="darwin linux windows" -output="./dist/{{.Dir}}_{{.OS}}_{{.Arch}}" ./cmd/kk/
